40f1791b747585105006c5239637446f72f69ab0
DevAna <organvidezanais@orange.fr>
2023-01-24 21:23:37 +0100

    Modification Route

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,24 @@
 import React from 'react';
-
-// Import de fichiers
-//import logo from './logo.svg';
-import './App.css';
-import Routes from './components/Routes/index.jsx';
-
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Home from './pages/Home';
+import Accommodation from './pages/Accommodation';
+import About from './pages/About';
+import Error404 from './pages/Error404';
+import NavBar from './components/Navbar';
+import Footer from './components/Footer';
 
 function App() {
   return (
-    <Routes />
+    <Router>
+      <NavBar />
+        <Routes>
+          <Route path='/' element={<Home />} />
+          <Route path='/Fiche-Logement/:logementId' element={<Accommodation />} />
+          <Route path='/A-propos' element={<About />} />
+          <Route path='*' element={<Error404 />} />
+        </Routes>
+      <Footer />
+    </Router>
   );
 }
 
